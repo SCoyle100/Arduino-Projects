327a1d115dbb766a1a8d45c4991c0b242430f4b9
SCoyle100 <83990984+SCoyle100@users.noreply.github.com>
2021-12-26 14:59:40 -0500

    Update skeeball_switch_nov_05.ino

--- skeeball_switch_nov_05.ino
+++ skeeball_switch_nov_05.ino
@@ -1,14 +1,3 @@
-// A basic everyday NeoPixel strip test program.
-
-// NEOPIXEL BEST PRACTICES for most reliable operation:
-// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
-// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
-// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
-// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
-//   connect GROUND (-) first, then +, then data.
-// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
-//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
-// (Skipping these may work OK on your workbench but can fail in the field)
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
